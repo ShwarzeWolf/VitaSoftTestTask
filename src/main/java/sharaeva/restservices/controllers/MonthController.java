3df810e32c688edf8c63d0c983932c5e0eafee7f
Kristin <1358365@DESKTOP-4FH9G41>
2020-07-22 22:14:04 +0300

    task 2 implemented

--- src/main/java/sharaeva/restservices/controllers/MonthController.java
+++ src/main/java/sharaeva/restservices/controllers/MonthController.java
@@ -9,7 +9,21 @@ public class MonthController {
 
     @GetMapping("/monthInfo")
     public String getMonthNameByNumber(@RequestParam(value = "monthNumber") int monthNumber) {
-        return "\"Я-Н-В-А-Р-Ь\"";
+        switch(monthNumber){
+            case 1: return "\"Я-Н-В-А-Р-Ь\"";
+            case 2: return "\"Ф-Е-В-Р-А-Л-Ь\"";
+            case 3: return "\"М-А-Р-Т\"";
+            case 4: return "\"А-П-Р-Е-Л-Ь\"";
+            case 5: return "\"М-А-Й\"";
+            case 6: return "\"И-Ю-Н-Ь\"";
+            case 7: return "\"И-Ю-Л-Ь\"";
+            case 8: return "\"А-В-Г-У-С-Т\"";
+            case 9: return "\"С-Е-Н-Т-Я-Б-Р-Ь\"";
+            case 10: return "\"О-К-Т-Я-Б-Р-Ь\"";
+            case 11: return "\"Н-О-Я-Б-Р-Ь\"";
+            case 12: return "\"Д-Е-К-А-Б-Р-Ь\"";
+            default: return "\"INCORRECT INPUT DATA\"";
+        }
     }
 
 }
