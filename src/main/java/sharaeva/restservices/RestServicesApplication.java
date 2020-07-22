9e098efa051fee17dc3e0edc2b95e9fad7c355ed
Kristin <1358365@DESKTOP-4FH9G41>
2020-07-22 21:23:43 +0300

    initial commit

---
+++ src/main/java/sharaeva/restservices/RestServicesApplication.java
@@ -0,0 +1,13 @@
+package sharaeva.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServicesApplication.class, args);
+    }
+
+}
